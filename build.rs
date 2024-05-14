61dde9fff2bf5ece0a7fd25b8e71740499db3a51
KillTheMule <KillTheMule@users.noreply.github.com>
2024-05-14 15:36:58 +0200

    Link against llvm stdlib

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
         println!("cargo:rustc-link-lib=static=pdfium");
         println!("cargo:rustc-link-search=native={}", env!("CARGO_MANIFEST_DIR"));
-        println!("cargo:rustc-link-lib=dylib=stdc++");
+        println!("cargo:rustc-link-lib=dylib=c++");
 }
