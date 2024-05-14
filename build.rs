fc5e2f048af827bcbe1ad74605f0f1460f919f30
KillTheMule <KillTheMule@users.noreply.github.com>
2024-05-14 13:19:21 +0200

    Try linking stdc++ ourselves

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
         println!("cargo:rustc-link-lib=static=pdfium");
         println!("cargo:rustc-link-search=native={}", env!("CARGO_MANIFEST_DIR"));
+        println!("cargo:rustc-link-lib=dylib=stdc++");
 }
